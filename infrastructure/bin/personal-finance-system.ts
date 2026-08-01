41698a575787108cdcb1bca815c1291b0f947dae
DavidCs9 <davidcastrosi@hotmail.com>
2026-08-01 14:57:02 -0600

    ci: add protected-main deployment workflow

--- infrastructure/bin/personal-finance-system.ts
+++ infrastructure/bin/personal-finance-system.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
+import { GitHubCiBootstrapStack } from '../lib/github-ci-bootstrap-stack.js';
 import { PersonalFinanceV1Stack } from '../lib/personal-finance-v1-stack.js';
 
 const app = new cdk.App();
@@ -15,4 +16,12 @@ new PersonalFinanceV1Stack(app, 'PersonalFinanceV1', {
     Environment: 'prod',
     ManagedBy: 'cdk',
   },
+});
+
+new GitHubCiBootstrapStack(app, 'PersonalFinanceCiBootstrap', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: 'us-east-2',
+  },
+  description: 'OIDC deployment access for the personal-finance GitHub Actions workflow.',
 });
---
+++ infrastructure/lib/github-ci-bootstrap-stack.ts
@@ -0,0 +1,45 @@
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+
+export class GitHubCiBootstrapStack extends Stack {
+  public constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    Object.entries({ Project: 'personal-finance-system', Environment: 'prod', ManagedBy: 'cdk' })
+      .forEach(([key, value]) => cdk.Tags.of(this).add(key, value));
+
+    const githubActionsOidcProvider = iam.OpenIdConnectProvider.fromOpenIdConnectProviderArn(
+      this,
+      'GitHubActionsOidcProvider',
+      `arn:${this.partition}:iam::${this.account}:oidc-provider/token.actions.githubusercontent.com`,
+    );
+    const deployRole = new iam.Role(this, 'GitHubDeployRole', {
+      roleName: 'personal-finance-v1-github-deploy',
+      description: 'Allows GitHub Actions from the protected main branch to deploy PersonalFinanceV1.',
+      maxSessionDuration: Duration.hours(1),
+      assumedBy: new iam.WebIdentityPrincipal(githubActionsOidcProvider.openIdConnectProviderArn, {
+        StringEquals: {
+          'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com',
+          'token.actions.githubusercontent.com:sub': 'repo:DavidCs9/personal-finance-system:ref:refs/heads/main',
+        },
+      }),
+    });
+    deployRole.addToPolicy(new iam.PolicyStatement({
+      actions: ['sts:AssumeRole', 'sts:TagSession'],
+      resources: [
+        `arn:${this.partition}:iam::${this.account}:role/cdk-hnb659fds-deploy-role-${this.account}-${this.region}`,
+        `arn:${this.partition}:iam::${this.account}:role/cdk-hnb659fds-file-publishing-role-${this.account}-${this.region}`,
+        `arn:${this.partition}:iam::${this.account}:role/cdk-hnb659fds-image-publishing-role-${this.account}-${this.region}`,
+        `arn:${this.partition}:iam::${this.account}:role/cdk-hnb659fds-lookup-role-${this.account}-${this.region}`,
+      ],
+    }));
+    deployRole.addToPolicy(new iam.PolicyStatement({
+      actions: ['ssm:GetParameter'],
+      resources: [`arn:${this.partition}:ssm:${this.region}:${this.account}:parameter/cdk-bootstrap/hnb659fds/version`],
+    }));
+
+    new cdk.CfnOutput(this, 'GitHubDeployRoleArn', { value: deployRole.roleArn });
+  }
+}
